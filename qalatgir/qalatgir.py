9e4300eab6473233dc65798bc8e97b4cbdad8c55
Aliakbar Salehbigdeli <al.ak.saleh@gmail.com>
2021-06-09 01:02:50 +0430

    Add linear interpolation

--- qalatgir/qalatgir.py
+++ qalatgir/qalatgir.py
@@ -1,14 +1,40 @@
-import datetime as dt
+import math
 
 import numpy as np
 import pandas as pd
 
 
-def missed(data, time):
-    for d in data:
-        if d['time'] == time:
-            return False
-    return True
+def get_consecutive_missing(values):
+    start = 0
+    null_found = False
+    misses = []
+    for i, v in enumerate(values):
+        if np.isnan(v) or not math.isfinite(v):
+            null_found = True
+            start = i
+        elif null_found:
+            misses.append((start, i))
+            null_found = False
+
+    return misses
+
+
+def average_interpolate(data, miss):
+
+    miss_count = miss[1] - miss[0]
+    start_value = data.iloc[miss[0] - 1]['value']
+    end_value = data.iloc[miss[1]]['value']
+    change = (end_value - start_value) / (miss_count + 1)
+
+    for j, i in enumerate(range(*miss)):
+        data.iloc[i] = (j + 1) * change + data.iloc[miss[0] - 1]
+
+
+def interpolate_missing(data, step, misses):
+    for miss in misses:
+        miss_count = miss[1] - miss[0]
+        if miss_count < 2 or (miss_count < 4 and step < 60):
+            average_interpolate(data, miss)
 
 
 def add_slots_for_missing(data, step):
@@ -24,5 +50,6 @@ def fill_missing(data, step):
     if not len(data):
         return data
     data = add_slots_for_missing(data, step)
-
+    misses = get_consecutive_missing(data['value'])
+    interpolate_missing(data, step, misses)
     return data
